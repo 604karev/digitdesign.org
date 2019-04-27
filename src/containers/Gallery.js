dd592876cbfd5152d55d0304eb50d61fa4eecc57
604karev <604karev@gmail.com>
2019-04-27 19:39:04 +0300

    added plugin Masonry

--- src/containers/Gallery.js
+++ src/containers/Gallery.js
@@ -4,115 +4,69 @@ import {fetchGallery} from '../actions'
 import * as R from 'ramda'
 import {Link} from 'react-router-dom'
 import './Gallery.sass'
+import Masonry from 'react-masonry-component';
+
+
 
 class Gallery extends Component {
-    state = {
-        width: window.innerWidth,
-    };
+
 
     componentDidMount = () => {
         this.props.fetchGallery();
-        window.addEventListener("resize", this.updateDimensions);
-    };
-    componentWillUnmount = () => {
-        window.removeEventListener("resize", this.updateDimensions);
-    };
-
-    updateDimensions = () => {
-        this.setState({
-            width: window.innerWidth
-        });
-    };
-    sliceTo = (arr, n) => {
-        let newArray = [];
-        for (let k = 0; k < n; k++) {
-            newArray[k] = []
-        }
-        for (let i = 0; i < arr.length; i += n) {
-            let tempArray = arr.slice(i, i + n);
-            for (let j = 0; j < n; j++) {
-                if (tempArray[j]) {
-                    newArray[j].push(tempArray[j])
-                }
-            }
-
-        }
-        return newArray
 
     };
-
 
     renderGallery = (item, index) => {
         return (
-            <div className="design-gallery-row-element" key={index}>
+            <div key={index} className="design-gallery-element">
                 <Link
                     className="design-gallery-row-element-link"
-                    to={'/' + item.category[0] + '/' + item.id}>
+                    to={`/${item.category[0]}/${item.id}`}>
                     <img
-                        className="design-gallery-row-element__image"
+                        className="img-fluid"
                         src={item.img}
                         alt={item.img}/>
-                    <span
-                        className="design-gallery-row-element-link__overlay">
-                        {item.category.map(
-                            (categoryEl, index) => {
-                                return (
-                                    categoryEl === 'web-design' ?
-                                        <span
-                                            key={index}
-                                            className="element-category">#Web design</span> : categoryEl === 'mobile' ?
-                                        <span key={index}
-                                              className="element-category">#Mobile</span> : categoryEl === 'art' ?
-                                            <span key={index}
-                                                  className="element-category">#Art</span> : categoryEl === 'icons' ?
-                                                <span key={index}
-                                                      className="element-category">#Icons</span> : categoryEl === 'identity' ?
-                                                    <span key={index}
-                                                          className="element-category">#Identity</span> : null
+                    <span className="design-gallery-row-element-link__overlay">
+                          {item.category.map(
+                              (categoryEl, index) => {
+                                  return (
+                                      categoryEl === 'web-design' ?
+                                          <span
+                                              key={index}
+                                              className="element-category">#Web design</span> : categoryEl === 'mobile' ?
+                                          <span key={index}
+                                                className="element-category">#Mobile</span> : categoryEl === 'art' ?
+                                              <span key={index}
+                                                    className="element-category">#Art</span> : categoryEl === 'icons' ?
+                                                  <span key={index}
+                                                        className="element-category">#Icons</span> : categoryEl === 'identity' ?
+                                                      <span key={index}
+                                                            className="element-category">#Identity</span> : null
 
-                                )
-                            })}
-                    </span>
+                                  )
+                              })}
+                      </span>
                 </Link>
             </div>
         )
     };
-    renderGalleryRow = (array, index) => (
-        <div className="design-gallery-row" key={index}>
-            {array.map((galleryItem, index) => this.renderGallery(galleryItem, index))}
-        </div>
-    );
 
 
     render() {
         const {gallery} = this.props;
-        let splitGallery = this.sliceTo(gallery, 5);
-
-        if (this.state.width <= 1199) {
-            splitGallery = this.sliceTo(gallery, 4);
-        }
-        if (this.state.width <= 991) {
-            splitGallery = this.sliceTo(gallery, 3);
-        }
-        if (this.state.width <= 768) {
-            splitGallery = this.sliceTo(gallery, 2);
-        }
-        if (this.state.width <= 480) {
-            splitGallery = this.sliceTo(gallery, 1);
-        }
-
-
         return (
-            <div className="design-gallery">
-                {splitGallery.map((array, index) => this.renderGalleryRow(array, index))}
-            </div>
+
+
+            <Masonry className="design-gallery" >
+                {gallery.map((array, index) => this.renderGallery(array, index))}
+            </Masonry>
         )
 
     }
 }
 
 const mapStateToProps = state => ({
-    gallery: R.values(state.gallery)
+    gallery: R.reverse(R.values(state.gallery))
 });
 
 const mapDispatchToProps = {
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/scss/bootstrap.scss'
 import './index.sass';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css'
+
 import 'babel-polyfill';
 import "isomorphic-fetch";
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+

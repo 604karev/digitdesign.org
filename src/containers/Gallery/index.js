1144af6714944b845c7a2292a304edf8e84e581a
604karev <604karev@gmail.com>
2019-05-19 15:51:26 +0300

    added absolute path's , change structure

---
+++ src/containers/Gallery/index.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {fetchGallery} from 'actions/index'
+import * as R from 'ramda'
+import {Link} from 'react-router-dom'
+import './index.sass'
+import Masonry from 'react-masonry-component';
+import {getGallery} from 'selectors';
+
+class Gallery extends Component {
+
+    componentDidMount = () => {
+        this.props.fetchGallery();
+    };
+
+    replaceSymbols = item =>
+        <span className="element-category" key={item}>
+            {`#${R.compose(
+                R.replace(/^./, R.toUpper),
+                R.replace('-', ' ',))(item)}`}
+                </span>;
+
+    renderGallery = (item, index) => {
+        return (
+            <div key={item.id} className="design-gallery-element">
+                <Link
+                    className="design-gallery__link"
+                    to={`/slider/${item.id}`}>
+                    <img
+                        className="img-fluid"
+                        src={item.img}
+                        alt={item.img}/>
+                    <span className="design-gallery__overlay">
+                           {R.map(this.replaceSymbols, item.category)}
+                    </span>
+                </Link>
+            </div>
+        )
+    };
+    render() {
+        const {gallery} = this.props;
+        return (
+            <Masonry
+                className="design-gallery">
+                {R.reverse(gallery.map((array, index) => this.renderGallery(array, index)))}
+            </Masonry>
+        )
+    }
+}
+const mapStateToProps = (state, ownProps) => ({
+    gallery: getGallery(state, ownProps),
+});
+
+const mapDispatchToProps = {
+    fetchGallery
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Gallery)

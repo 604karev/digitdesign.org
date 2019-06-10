3c886c11cf220eb8b0577a8a38de690ce2d495a0
604karev <604karev@gmail.com>
2019-06-10 16:50:43 +0300

    fixed bug this carousel

--- src/containers/CarouselWrapper/index.js
+++ src/containers/CarouselWrapper/index.js
@@ -2,22 +2,25 @@ import React from "react";
 import {Link, Route} from "react-router-dom";
 import Carousel from 'components/Carousel';
 import {connect} from 'react-redux';
-import {getGalleryItemById, getGalleryLength} from 'selectors'
+import {getGalleryItemById} from 'selectors'
 import Navigation from 'components/Navigation';
 import NavigationSlider from 'components/Navigation/NavigationSlider'
 import SwitchWithSlide from 'components/Carousel/Slider/SwitchWithSlide';
 import './index.sass'
 
-const CarouselWrapper = ({portfolioItem, galleryLength}) => {
+const CarouselWrapper = ({portfolioItem, gallery}) => {
 
-    const getSliderId = (currentId) => {
-        if (currentId > galleryLength) {
-            currentId = 1
+    const getItem = (key, i) => {
+        let keys = Object.keys(gallery);
+        let index = keys.indexOf(key);
+        index = index + i;
+        if (i === 1 && index === keys.length) {
+            index = 0
         }
-        if (currentId <= 0) {
-            currentId = galleryLength
+        if (i === -1 && index === -1) {
+            index = keys.length - 1
         }
-        return currentId
+        return gallery[keys[index]] && gallery[keys[index]].id;
     };
 
     return (
@@ -30,17 +33,18 @@ const CarouselWrapper = ({portfolioItem, galleryLength}) => {
                     <Route exact path='/carousel/:id' component={Carousel}/>
                 </SwitchWithSlide>
                 <Link className="slick-arrow slick-next"
-                      to={`/carousel/${portfolioItem && getSliderId(portfolioItem.id - 1)}`}/>
+                      to={`/carousel/${portfolioItem && getItem(portfolioItem.id.toString(), -1)}`}/>
                 <Link className="slick-arrow slick-prev"
-                      to={`/carousel/${portfolioItem && getSliderId(portfolioItem.id + 1)}`}/>
+                      to={`/carousel/${portfolioItem && getItem(portfolioItem.id.toString(), +1)}`}/>
+
             </section>
         </div>
     )
 };
 
 const mapStateToProps = (state) => ({
-    portfolioItem: getGalleryItemById(state, state.galleryPageId.id),
-    galleryLength: getGalleryLength(state),
+    gallery: state.gallery,
+    portfolioItem: getGalleryItemById(state, state.galleryPageId.id)
 });
 
 export default connect(mapStateToProps)(CarouselWrapper)
